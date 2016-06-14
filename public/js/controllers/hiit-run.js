fca570bcbc4b91c0102bba18b7b3a7ecaf112e33
briacp <briacp@gmail.com>
2016-06-14 21:57:23 +0200

    Semi-functionnal running window

--- public/js/controllers/hiit-run.js
+++ public/js/controllers/hiit-run.js
@@ -4,64 +4,142 @@
 angular.module('hiitTimerApp').controller('runCtrl', function ($scope, $rootScope, $window, $log, $interval, durationFactory) {
     'use strict';
     var runInterval;
+    var setIndex;
+    var actionIndex;
 
     $rootScope.$on('runEvent', function (ev, args) {
-        $log.debug('runEvent', ev, args);
         _initRun(args.runType, args.runData);
     });
 
+    var _ucfirst = function (s) {
+        return s.charAt(0).toUpperCase() + s.slice(1);
+    };
+
     var _initRun = function (type, runData) {
         $log.debug('_initRun', type, runData);
-        var run = {};
-        angular.copy(runData, run);
-        $scope.run = run;
-        $scope.run.type = type;
+        var data = {};
+        angular.copy(runData, data);
 
-        var totalSeconds = durationFactory.workoutDuration(run, true);
-
-        run.sets.push({
-            name: 'Prepare!',
-            repetitions: 1,
-            actions: [{
-                name: 'Prepare',
-                time: 5,
-                color: '#FFFFFF'
-            }]
-        });
-
-        $scope.countDown = totalSeconds;
-        $scope.timer = {
-            running: false,
-            totalSeconds: parseInt(totalSeconds, 10),
-            timeLeft: durationFactory.sec2minsec(totalSeconds),
-            currSeconds: 0,
+        var run = {
+            title: _ucfirst(type) + ' - ' + runData.name,
+            type: type,
+            data: data,
+            workout: {},
+            sets: [],
+            actions: []
         };
-        $scope.timer.percent = 98; // $scope.timer.currSeconds == 0 ? 0.1 :$scope.timer.currSeconds / $scope.timer.totalSeconds * 100;
 
-        $scope.actionCountDown = '01:22';
-        $scope.currentRound = 1;
-        $scope.totalRounds = 8;
-        $scope.setTimeLeft = '2:33';
+        var totalSeconds = durationFactory.workoutDuration(run.data, true);
+
+        //run.data.sets.unshift({
+        //    name: 'Prepare!',
+        //    repetitions: 1,
+        //    actions: [{
+        //        name: 'Prepare',
+        //        time: 5,
+        //        color: '#FFFFFF'
+        //    }]
+        //});
+
+        // Workout ------------------------------------------------------------
+        run.workout.totalSeconds = totalSeconds;
+        run.workout.timeLeft = totalSeconds;
+        run.workout.percent = 0.01;
+        run.workout.currSeconds = 0;
+
+        // Sets ---------------------------------------------------------------
+        setIndex = 0;
+        var currentSet = run.data.sets[setIndex];
+        run.set = currentSet;
+        run.set.index = setIndex;
+        run.set.previous = null;
+        run.set.next = run.data.sets[setIndex + 1] || null;
+        run.set.round = 1;
+        run.set.timeLeft = durationFactory.setDuration(currentSet, true);
+
+        // Actions ------------------------------------------------------------
+        actionIndex = 0;
+        run.action = currentSet.actions[actionIndex];
+        run.action.index = actionIndex;
+        run.action.previous = null;
+        run.action.next = currentSet[actionIndex + 1] || null;
+        run.action.timeLeft = run.action.time;
+
+        $scope.run = run;
 
         $('#runModal').modal();
     };
 
-    $scope.stopRun = function () {
-        $interval.cancel(runInterval);
+    var _nextSet = function () {};
+
+    var _nextAction = function () {
+        var run = $scope.run;
+        var currentSet = run.set;
+        var actionIndex = run.action.index + 1;
+        run.action = currentSet.actions[actionIndex];
+        if (!run.action) {
+            _nextSet();
+            return;
+        }
+        run.action.index = actionIndex;
+        run.action.previous = currentSet.actions[actionIndex - 1];
+        run.action.next = currentSet[actionIndex + 1] || null;
+        run.action.timeLeft = run.action.time;
+    };
+
+    var _intervalRun = function () {
+        $scope.run.timeLeft -= 1;
+
+        var run = $scope.run;
+
+        // Workout Update -----------------------------------------------------
+        run.workout.currSeconds += 1;
+        run.workout.timeLeft -= 1;
+        run.workout.percent = run.workout.currSeconds / run.workout.totalSeconds * 100;
+
+        // Set Update ---------------------------------------------------------
+        run.set.timeLeft -= 1;
+
+        // Action Update ------------------------------------------------------
+        run.action.timeLeft -= 1;
+
+        if (run.action.timeLeft === 0) {
+            _nextAction();
+        }
+
+        // Check for finish condition -----------------------------------------
+        if (run.workout.timeLeft === 0) {
+            console('DONE!');
+            $interval.cancel(runInterval);
+        }
+        if (run.set.timeLeft <= 5) {
+            // Play sound
+            $log.debug('TODO - Play sound.');
+        }
+
+        $log.debug(run, run.workout.timeLeft);
     };
 
     $scope.startRun = function () {
-        $scope.timer.running = true;
-        runInterval = $interval(function () {
-            $scope.timer.timeLeft -= 1;
-            $scope.timer.currSeconds += 1;
-            $scope.countDown -= 1;
-
-            $log.debug($scope.timer, $scope.countDown);
-        }, 1000);
+        $scope.run.timerRunning = true;
+        if (runInterval) {
+            return;
+        }
+        runInterval = $interval(_intervalRun, 1000);
     };
+
+    $scope.stopRun = function () {
+        $scope.run.timerRunning = false;
+        $interval.cancel(runInterval);
+        runInterval = null;
+    };
+
 
     $scope.stepForward = function () {
         $log.debug('nextStep');
+    };
+
+    $scope.sec2minsec = function (s) {
+        return durationFactory.sec2minsec(s);
     };
 });
